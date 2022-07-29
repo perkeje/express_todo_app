a630c1ce39bc6545742064f5bb802e0ea57280eb
Juraj Perić <juraj.peric@gmail.com>
2022-07-29 13:43:15 +0200

    Added CORS

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,12 +1,17 @@
 var express = require("express");
+var cors = require("cors");
 var router = express.Router();
 var register = require("./auth/register");
 var login = require("./auth/login");
 var tasks = require("./tasks/tasks");
 var { authenticateToken } = require("../middlewares/auth");
 
+router.use(cors());
 router.use(express.json());
 
+router.get("/", (req, res) => {
+  res.json({ msg: "home" });
+});
 router.use("/login", login);
 router.use("/register", register);
 router.use("/tasks", authenticateToken);
